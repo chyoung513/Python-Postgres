7b518ac2ebdaa4dfc3073fdf69af4b729fa5d780
chyoung513 <jxy5130@gmail.com>
2018-08-26 22:02:55 +0900

    create nested dict example
    execise in dataquest
    - red table column and made nested dictionary

---
+++ create nested dict.py
@@ -0,0 +1,42 @@
+from psycopg2.extensions import AsIs
+#connect to Postgres
+conn = psycopg2.connect(dbname="", user="", password="")
+cur = conn.cursor()
+
+readable_description = {}
+#table_names is list of names for tables
+for table in table_names:
+    cur.execute("SELECT * FROM %s LIMIT 0", [AsIs(table)])
+    #create nest dictionary
+    readable_description[table] = dict(
+        columns=[
+            dict(
+                name=col.name,
+                type=type_mappings[col.type_code],
+                length=col.internal_size
+            )
+            for col in cur.description
+        ]
+    )
+print(readable_description)
+
+
+
+# the output of dictionary as bellow
+{
+    "homeless":
+        {
+            columns: [
+                {
+                    name: "id"
+                    type: "int4"
+                    internal_size: 4
+                },
+                {
+                    name: "year",
+                    type: "date",
+                    internal_size: 4
+                }
+            ]
+            }
+    }
